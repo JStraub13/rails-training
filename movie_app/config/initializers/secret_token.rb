8b078471ffa29b8cddf9d13211cf4a052be946c4
Jared <jared.straub@appfolio.com>
2014-01-06 21:53:37 -0800

    was having issues with old directory so just created new one for more practice

---
+++ movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = 'b0b5b2b933380a75bd36e04810e0579182f64ef83d31dac0a9df4fc45105a183877f7eb1cafb8812958482620392f88957cd76b3d254714ae4b9f12692e76c20'
