e87bb399e17ce2d5b2e6b5a679798751d4dd3b67
Jared <jared.straub@appfolio.com>
2014-01-07 22:35:36 -0800

    Chapters 11 thru 13

--- movie_app/config/routes.rb
+++ movie_app/config/routes.rb
@@ -1,7 +1,8 @@
 MovieApp::Application.routes.draw do
-  get "movies" =>"movies#index"
-  get "movies/:id" => "movies#show", as: "movie"  
-  root "movies#index"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-
+  # get "movies" =>"movies#index"
+  # get "movies/:id" => "movies#show", as: "movie"  
+    root "movies#index"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id" => "movies#update"
+  resources :movies
 end
