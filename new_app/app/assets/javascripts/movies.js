bb4a5dc971b18489c2b1431bbb2da12b1542f5cb
Jared <jared.straub@appfolio.com>
2014-02-19 21:34:19 -0800

    small changes to search results

--- new_app/app/assets/javascripts/movies.js
+++ new_app/app/assets/javascripts/movies.js
@@ -1,5 +1,8 @@
 
  $(function () {  
+ 	var minlength = 3;
+
+
       $('#movies_search input').keyup(function () {  
         $.get($('#movies_search').attr('action'), $('#movies_search').serialize(), null, 'script');  
         return false;  
