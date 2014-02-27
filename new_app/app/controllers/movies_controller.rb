746eff862786ffc3f431591d867686a81490836f
Jared <jared.straub@appfolio.com>
2014-02-26 17:17:13 -0800

    validations chapter completed

--- new_app/app/controllers/movies_controller.rb
+++ new_app/app/controllers/movies_controller.rb
@@ -16,10 +16,13 @@ class MoviesController < ApplicationController
   end
   
   def update
-    @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
+  @movie = Movie.find(params[:id])
+  if @movie.update(movie_params)
     redirect_to @movie
+  else
+    render :edit
   end
+end
 
   def new
     @movie = Movie.new
@@ -27,9 +30,12 @@ class MoviesController < ApplicationController
   
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
   end
+end
   
   def destroy
     @movie = Movie.find(params[:id])
